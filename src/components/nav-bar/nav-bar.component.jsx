1e51ca42f332f4988d56a61d932b1672a9fa484f
Kyrolos <pero.kero01@gmail.com>
2020-03-17 22:40:02 +0200

    adding form, implementing simple validation, adding nav-bar

---
+++ src/components/nav-bar/nav-bar.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => (
+    <div className='header'>
+      <div className='options'>
+        <p className='option' >
+          Sign-in
+        </p>
+        <p className='option' >
+          Sing-Up
+        </p>
+      </div>
+    </div>
+);
+
+export default Header;
